c0317a993de84d67315fd9b6650897a1eaac0131
crapboy <giulianopertile@gmail.com>
2021-09-18 23:57:21 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading;
+using Figgle;
+using Spectre.Console;
+
+namespace FizzBuzzAscii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            PrinterAscii printer = new PrinterAscii();
+            printer.Print("Welcome", "[mediumpurple2]");
+            printer.Print("to the", "[bold lightskyblue1]");
+            printer.Print("FizzBuzz Generator!", "[bold darkolivegreen2]");
+
+
+
+
+            Console.WriteLine();
+
+
+
+            var fizzBuzz = AnsiConsole.Prompt(
+            new TextPrompt<int>("[bold yellow2]From 1 to what number you want to generate the FizzBuzz?[/]")
+                .Validate(fb =>
+                {
+                    return fb switch
+                    {
+                        <= 0 => ValidationResult.Error("[red]Can't be less or equal to zero[/]"),
+                        _ => ValidationResult.Success(),
+                    };
+                }));
+
+
+            for (int i = 0; i <= fizzBuzz; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    var style = "[bold yellow]";
+                    printer.Print("FizzBuzz", style);
+                }
+                else if (i % 3 == 0)
+                {
+                    var style = "[bold green]";
+
+                    printer.Print("Fizz", style);
+                }
+                else if (i % 5 == 0)
+                {
+                    var style = "[bold blue]";
+
+                    printer.Print("Buzz", style);
+                }
+                else
+                {
+                    var style = "[bold purple]";
+
+                    printer.Print(i.ToString(), style);
+                }
+                Thread.Sleep(100);
+
+            }
+
+            var panel = new Panel($"[bold lime]{FiggleFonts.Standard.Render("That's all folks!")}[/]");
+            panel.Border = BoxBorder.Double;
+            AnsiConsole.Render(panel);
+
+        }
+    }
+
+    class PrinterAscii
+    {
+        public void Print(String value, String style)
+        {
+            AnsiConsole.Markup($"{style}{FiggleFonts.Standard.Render(value)}[/]");
+        }
+    }
+
+}
