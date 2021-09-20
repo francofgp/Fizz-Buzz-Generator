51b309b0b0a4890bbc361f85d3a84e502a15afba
crapboy <giulianopertile@gmail.com>
2021-09-20 19:59:11 -0300

    se modificó String a string, y ahora usó var consistentemente

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace FizzBuzzAscii
         static void Main(string[] args)
         {
 
-            PrinterAscii printer = new PrinterAscii();
+            var printer = new PrinterAscii();
             printer.Print("Welcome", "[mediumpurple2]");
             printer.Print("to the", "[bold lightskyblue1]");
             printer.Print("FizzBuzz Generator!", "[bold darkolivegreen2]");
@@ -74,7 +74,7 @@ namespace FizzBuzzAscii
         /// </summary>
         /// <param name="value">The string you want to print</param>
         /// <param name="style">The style according to the spectro.console styling nuget package</param>
-        public void Print(String value, String style)
+        public void Print(string value, string style)
         {
             AnsiConsole.Markup($"{style}{FiggleFonts.Standard.Render(value)}[/]");
         }
